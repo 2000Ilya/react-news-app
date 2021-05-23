1c05191704892d8bce61694be8ddaf8bc8f0a52c
Ilya Konik <konick.ilya@yandex.ru>
2021-05-24 04:32:51 +0500

    Header, NewsFeed, Themes, Footer components were added. Request function in useEffect hook was added. Handlers on themes and showing screen were added. Dynamic text color object was added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,48 @@
+import { useEffect, useState } from "react";
+
+import Header from "./components/Header/Header";
+import NewsFeed from "./components/NewsFeed/NewsFeed";
+import Footer from "./components/Footer/Footer";
+import Themes from "./components/Themes/Themes";
+
+import { getTheme } from "./api";
+
+import "./App.scss";
+
 const App = () => {
-  return <div className="App"></div>;
+  const [theme, setTheme] = useState({});
+  const [isNewsFeedShowing, setNewsFeedShowing] = useState(true);
+  const [themeName, setThemeName] = useState("light");
+
+  const appStyle = {
+    color: theme.textColor
+  };
+
+  const headerTitle = isNewsFeedShowing ? "Новости" : "Темы";
+
+  useEffect(() => {
+    getTheme(themeName).then(setTheme);
+  }, [themeName]);
+
+  const changeDisplayShowing = isNewsFeedShowing => {
+    setNewsFeedShowing(isNewsFeedShowing);
+  };
+
+  const changeTheme = themeName => {
+    setThemeName(themeName);
+  };
+
+  return (
+    <div className="App" style={appStyle}>
+      <Header headerTitle={headerTitle} themeStyle={theme} />
+      {isNewsFeedShowing ? (
+        <NewsFeed themeStyle={theme} />
+      ) : (
+        <Themes themeStyle={theme} changeTheme={changeTheme} />
+      )}
+      <Footer themeStyle={theme} setNewsFeedShowing={changeDisplayShowing} />
+    </div>
+  );
 };
 
 export default App;
