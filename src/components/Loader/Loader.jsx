305286dfcad2aa01100a0fa37aa488fde3f6ae62
Ilya Konik <konick.ilya@yandex.ru>
2021-05-27 14:26:37 +0500

    Styled-components lib added to this component, border style is dynamically depends on themeStyle.

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,7 +1,38 @@
-import "./Loader.scss";
+import styled, { keyframes } from "styled-components";
 
-const Loader = () => {
-  return <div className="loader"></div>;
+const Loader = ({ themeStyle }) => {
+  const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  
+  to {
+    transform: rotate(360deg);
+  }
+  `;
+
+  const LoaderElement = styled.div`
+    display: inline-block;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &:after {
+      content: " ";
+      display: block;
+      width: 50%;
+      height: 50%;
+      border-radius: 50%;
+      border: 5px solid ${themeStyle.secondColor};
+      border-color: ${themeStyle.secondColor} transparent
+        ${themeStyle.secondColor} transparent;
+      animation: ${rotate} 1.2s linear infinite;
+    }
+  `;
+
+  return <LoaderElement />;
 };
 
 export default Loader;
